153b3bbdc47db9380a1176f0648c1062ebd89a6a
Pedro Passos <pedropassos.yt@gmail.com>
2025-01-11 13:15:50 -0500

    background looks right but doesn't take up entire screen

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,28 @@
 <template>
-  <div id="app">
+  <div id="app" :style="gradientStyle">
     <img alt="Vue logo" src="./assets/logo.png">
     <h1>Hello</h1>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      gradientStyle: {
+        background: 'linear-gradient(0deg, #D63B19 0%, #F77D25 52%, #E61862 100%)',
+        height: '100vh', // Optional: Make sure the gradient fills the entire viewport
+      },
+    };
+  },
+};
+</script>
+
+<style>
+/* Optional: Add styles for other elements */
+h1 {
+  color: white;
+  text-align: center;
+  margin-top: 20px;
+}
+</style>
