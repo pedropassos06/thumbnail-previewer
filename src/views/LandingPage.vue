0f291dd2d1a51dae9f9b142ac9d8b1e1706ccab2
Pedro Passos <pedropassos.yt@gmail.com>
2025-01-17 19:22:20 -0500

    creating landing page background

---
+++ src/views/LandingPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="bg">
+    <h1>Preview Your Thumbnail!</h1>
+  </div>
+</template>
+
+<style>
+.bg {
+  background: linear-gradient(to bottom right, #D63B19 0%, #E61862 52%, #F77D25 100%);
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
