c2612c8e78243d1c8af888ee38cb7c153f26a7dc
Pedro Passos <pedropassos.yt@gmail.com>
2025-01-20 09:49:46 -0500

    fix button stroke

--- src/components/ActionButton.vue
+++ src/components/ActionButton.vue
@@ -1,71 +1,66 @@
 <template>
-    <button
-      :style="buttonStyles"
-      class="action-button"
-      @click="handleClick"
-    >
-      <slot />
-    </button>
+  <button :style="buttonStyles" class="action-button" @click="handleClick">
+    <slot />
+  </button>
 </template>
-  
+
 <script>
-  export default {
-    name: "ActionButton",
-    props: {
-      width: {
-        type: String,
-        default: "auto",
-      },
-      height: {
-        type: String,
-        default: "auto",
-      },
-      backgroundColor: {
-        type: String,
-        default: "#FF0000",
-      },
-      textColor: {
-        type: String,
-        default: "#FFFFFF",
-      },
-      stroke: {
-        type: String,
-        default: "none",
-      },
-      hasDropShadow: {
-        type: Boolean,
-        default: true,
-      },
+export default {
+  name: "ActionButton",
+  props: {
+    width: {
+      type: String,
+      default: "auto",
     },
-    computed: {
-      buttonStyles() {
-        return {
-          width: "100%",
-          height: this.height,
-          backgroundColor: this.backgroundColor,
-          color: this.textColor,
-		  border: "none",
-		  stroke: this.stroke,
-          borderRadius: "10px",
-          padding: "10px 20px",
-          cursor: "pointer",
-          boxShadow: this.hasDropShadow
-            ? "0px 2px 2px rgba(0, 0, 0, 0.10)"
-            : "none",
-          transition: "all 0.3s ease",
-        };
-      },
+    height: {
+      type: String,
+      default: "auto",
     },
-    methods: {
-      handleClick(event) {
-        this.$emit("click", event);
-      },
+    backgroundColor: {
+      type: String,
+      default: "#FF0000",
     },
-  };
+    textColor: {
+      type: String,
+      default: "#FFFFFF",
+    },
+    stroke: {
+      type: String,
+      default: null,
+    },
+    hasDropShadow: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  computed: {
+    buttonStyles() {
+      return {
+        width: "100%",
+        height: this.height,
+        backgroundColor: this.backgroundColor,
+        color: this.textColor,
+        border: this.stroke ? `1px solid ${this.stroke}` : "none",
+        borderRadius: "10px",
+        padding: "10px 20px",
+        cursor: "pointer",
+        boxShadow: this.hasDropShadow
+          ? "0px 2px 2px rgba(0, 0, 0, 0.10)"
+          : "none",
+        transition: "all 0.3s ease",
+      };
+    },
+  },
+  methods: {
+    handleClick(event) {
+      this.$emit("click", event);
+    },
+  },
+};
 </script>
 
 <style scoped>
-  .action-button:hover {
-    opacity: 0.8;
-  }
-</style>  
+.action-button:hover {
+  opacity: 0.8;
+}
+</style>
