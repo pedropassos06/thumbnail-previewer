d3872ead64a8c36b170b0215f3971668ed7fa5bc
Pedro Passos <pedropassos.yt@gmail.com>
2025-01-21 19:19:51 -0500

    create view for when dragging over to drop zone

--- src/components/ValidDropZone.vue
+++ src/components/ValidDropZone.vue
@@ -6,10 +6,17 @@
 		@dragleave="onDragLeave"
 		@drop.prevent="onDrop"
 	>
-        <div v-for="file in files" :key="file.name" class="uploaded-image-card-container">
-            <UploadedImageCard :fileName="file.name" />
+        <div class="drop-zone__content">
+            <div v-if="isDragging" class="drop-zone__drag-content">
+                <p class="drop-zone-subheader">You can drop you image now</p>
+            </div>
+            <div v-else class="drop-zone__no-drag-content">
+                <div v-for="file in files" :key="file.name" class="uploaded-image-card-container">
+                    <UploadedImageCard :fileName="file.name" />
+                </div>
+                <p class="drop-zone-subheader">Click done or <a class="hyperlink">browse again</a></p>
+            </div>
         </div>
-		<p class="drop-zone-subheader">Click done or <a class="hyperlink">browse again</a></p>
 	</div>
 </template>
 
@@ -52,9 +59,14 @@ export default {
 </script>
 
 <style scoped>
-.drop-zone-subheader {
+.drop-zone {
     color: #6C606C;
-    margin-bottom: 20px;
+    border: 1px dashed #14AE5C;
+    background-color: #F7FEF6;
+    border-radius: 10px;
+    height: 90%;
+    width: 100%;
+    padding: 10% 0%;
 }
 
 .hyperlink {
@@ -66,28 +78,22 @@ export default {
     cursor: pointer;
 }
 
+
 .drop-zone--drag-over {
     border-color: #007bff;
     background-color: #e6f7ff;
-}
-
-.drop-zone {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-between;
-    color: #6C606C;
-    border: 1px dashed #14AE5C;
-    background-color: #F7FEF6;
-    border-radius: 10px;
-    height: 90%;
-    width: 100%;
-    padding: 10% 0%;
 }
 
 .uploaded-image-card-container {
     display: flex;
     justify-content: center;
     margin-bottom: 20px;
+}
+
+.drop-zone__content {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
 }
 </style>
