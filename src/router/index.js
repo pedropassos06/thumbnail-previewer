f864ab4fa7de57ee2ce49e7b74bc8ab72c7b6e1d
Pedro Passos <pedropassos.yt@gmail.com>
2025-01-24 17:30:14 -0500

    create basic routes

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,10 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/', component: () => import('../views/LandingPage.vue'),
+      path: '/preview', component: () => import('../views/PreviewThumbnail.vue')
+    }
   ],
 })
 
