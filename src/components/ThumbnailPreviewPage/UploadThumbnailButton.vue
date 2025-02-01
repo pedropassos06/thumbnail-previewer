61eb5feed294625eb309d398bfd27725835b95c6
Pedro Passos <pedropassos.yt@gmail.com>
2025-02-01 18:24:33 -0500

    created upload thumbnail button

---
+++ src/components/ThumbnailPreviewPage/UploadThumbnailButton.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="upload-thumbnail-button">
+        <p class="add-icon">+</p>
+    </div>
+</template>
+<script>
+export default {
+    name: "UploadThumbnailButton",
+}
+</script>
+<style>
+.upload-thumbnail-button {
+    background-color: #D5D5D5;
+    border-radius: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+}
+
+.add-icon {
+    font-size: 25px;
+    color: #6F6F6F;
+}
+</style>
