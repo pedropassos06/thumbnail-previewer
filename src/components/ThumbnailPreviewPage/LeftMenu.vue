bc723c174e66cda4a7efef96ed8b9627adbe69eb
Pedro Passos <pedropassos.yt@gmail.com>
2025-02-01 16:02:14 -0500

    begin creating LeftMenu component

---
+++ src/components/ThumbnailPreviewPage/LeftMenu.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="left-menu">
+        <LeftMenuToggleButton />
+    </div>
+</template>
+
+<script>
+import LeftMenuToggleButton from "@/components/ThumbnailPreviewPage/LeftMenuToggleButton.vue";
+export default {
+    name: "LeftMenu",
+    components: {
+        LeftMenuToggleButton,
+    },
+}
+</script>
+
+<style>
+.left-menu {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    color: black;
+    border: 1px solid #E6E7EA;
+    border-radius: 15px;
+    padding: 1%;
+}
+</style>
