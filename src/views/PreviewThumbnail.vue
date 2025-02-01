891741627c2258e6c14874c7a6f9dbb755939a2c
Pedro Passos <pedropassos.yt@gmail.com>
2025-02-01 16:02:01 -0500

    begin creating this page, added LeftMenu to it

--- src/views/PreviewThumbnail.vue
+++ src/views/PreviewThumbnail.vue
@@ -1,3 +1,31 @@
 <template>
-    <h1>Welcome to the Preview screen</h1>
+    <div class="preview-thumbnail-page">
+        <LeftMenu class="left-menu"/>
+    </div>
 </template>
+
+<script>
+import LeftMenu from "@/components/ThumbnailPreviewPage/LeftMenu.vue";
+
+export default {
+    components: {
+        LeftMenu,
+    },
+}
+</script>
+
+<style>
+.preview-thumbnail-page {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    background-color: #f5f5f5;
+    color: black;
+}
+
+.left-menu {
+    width: 20%;
+    height: 90%;
+}
+</style>
