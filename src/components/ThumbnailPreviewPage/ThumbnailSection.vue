8d5883448d14f5ae5873bbe5d3faf54e5d225e0a
Pedro Passos <pedropassos.yt@gmail.com>
2025-02-01 18:24:56 -0500

    add UploadThumbnailButton and allow to search files

--- src/components/ThumbnailPreviewPage/ThumbnailSection.vue
+++ src/components/ThumbnailPreviewPage/ThumbnailSection.vue
@@ -1,11 +1,36 @@
 <template>
     <div class="thumbnail-section">
         <h3 class="title">Thumbnails</h3>
+        <input
+			type="file"
+			ref="fileInput"
+			accept=".jpg,.jpeg,.png"
+			class="file-input"
+			@change="onFileChange"
+			multiple
+			hidden
+		/>
+        <UploadThumbnailButton 
+            @click="openFileExplorer"
+        />
     </div>
 </template>
 <script>
+import UploadThumbnailButton from '@/components/ThumbnailPreviewPage/UploadThumbnailButton.vue';
+
 export default {
-    
+    components: {
+        UploadThumbnailButton,
+    },
+    methods: {
+        openFileExplorer() {
+            this.$refs.fileInput.click();
+        },
+        onFileChange(event) {
+            const files = event.target.files;
+            this.$emit('files-dropped', files);
+        },
+    },
 }
 </script>
 <style>
