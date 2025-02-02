a9a3c2709349d3383ddb8dfc9dc2b52a2873724c
Pedro Passos <pedropassos.yt@gmail.com>
2025-02-02 11:17:04 -0500

    add UploadedThumbnail component

---
+++ src/components/ThumbnailPreviewPage/UploadedThumbnail.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="uploaded-thumbnail">
+        <img src="https://via.placeholder.com/150" alt="thumbnail" />
+    </div>
+</template>
+
+<script>
+export default {
+    name: "UploadedThumbnail",
+    props: {
+        thumbnail: {
+            type: Thumbnail,
+            default: () => {},
+        },
+    },
+}
+</script>
+
+<style>
+    
+</style>
