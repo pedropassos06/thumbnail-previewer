00348ecc3b65e57fadffd8b84f91504dc31d2ffb
Pedro Passos <pedropassos.yt@gmail.com>
2025-02-06 22:06:20 -0500

    create add title button

---
+++ src/components/AddTitleButton.vue
@@ -0,0 +1,26 @@
+<template>
+    <button class="add-title-button"><font-awesome-icon class="plus-icon" icon="fa-solid fa-plus" /></button>
+</template>
+<script>
+export default {
+    name: "AddTitleButton",
+}
+</script>
+<style>
+.add-title-button {
+    border: none;
+    width: 100%;
+    background-color: #D5D5D5;
+    border-radius: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    padding: 2%;
+}
+
+.plus-icon {
+    font-size: 17px;
+    color: #6F6F6F;
+}
+</style>
