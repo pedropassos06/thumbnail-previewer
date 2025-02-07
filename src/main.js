e122491d34e64ad5c5b476f4f40b41ea986953b9
Pedro Passos <pedropassos.yt@gmail.com>
2025-02-06 20:12:37 -0500

    import fa icons into project

--- src/main.js
+++ src/main.js
@@ -4,8 +4,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
 import router from './router'
+import { FontAwesomeIcon } from '@/components/icons/fontAwesome'
 
 const app = createApp(App)
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.use(store);
 app.use(router)
