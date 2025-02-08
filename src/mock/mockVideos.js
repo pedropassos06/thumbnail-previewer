2577b88a7eb14419748c6df014c591ef3f0a7fa2
Pedro Passos <pedropassos.yt@gmail.com>
2025-02-08 17:38:37 -0500

    create list of mock videos

---
+++ src/mock/mockVideos.js
@@ -0,0 +1,67 @@
+const mockVideos = [
+    {
+        id: 1,
+        thumbnail: '@/assets/mock/$1 vs $500,000 Experiences!.png',
+        title: '$1 vs $500,000 Experiences!',
+        channelProfilePic: '@/assets/mock/MrBeastChannelPic.jpg',
+        channelName: 'Mr. Beast',
+    },
+    {
+        id: 2,
+        thumbnail: '@/assets/mock/$10,000 Every Day You Survive In A Grocery Store.png',
+        title: '$10,000 Every Day You Survive In A Grocery Store',
+        channelProfilePic: '@/assets/mock/MrBeastChannelPic.jpg',
+        channelName: 'Mr. Beast',
+    },
+    {
+        id: 3,
+        thumbnail: '@/assets/mock/2,000 People Fight For $5,000,000.png',
+        title: '2,000 People Fight For $5,000,000',
+        channelProfilePic: '@/assets/mock/MrBeastChannelPic.jpg',
+        channelName: 'Mr. Beast',
+    },
+    {
+        id: 4,
+        thumbnail: '@/assets/mock/7 Days Stranded At Sea.png',
+        title: '7 Days Stranded At Sea',
+        channelProfilePic: '@/assets/mock/MrBeastChannelPic.jpg',
+        channelName: 'Mr. Beast',
+    },
+    {
+        id: 5,
+        thumbnail: '@/assets/mock/7 Days Stranded On An Island.png',
+        title: '7 Days Stranded On An Island',
+        channelProfilePic: '@/assets/mock/MrBeastChannelPic.jpg',
+        channelName: 'Mr. Beast',
+    },
+    {
+        id: 6,
+        thumbnail: '@/assets/mock/50 YouTubers Fight For $1,000,000.png',
+        title: '50 YouTubers Fight For $1,000,000',
+        channelProfilePic: '@/assets/mock/MrBeastChannelPic.jpg',
+        channelName: 'Mr. Beast',
+    },
+    {
+        id: 7,
+        thumbnail: '@/assets/mock/100 Identical Twins Fight For $250,000.png',
+        title: '100 Identical Twins Fight For $250,000',
+        channelProfilePic: '@/assets/mock/MrBeastChannelPic.jpg',
+        channelName: 'Mr. Beast',
+    },
+    {
+        id: 8,
+        thumbnail: '@/assets/mock/Every Country On Earth Fights For $250,000!.png',
+        title: 'Every Country On Earth Fights For $250,000!',
+        channelProfilePic: '@/assets/mock/MrBeastChannelPic.jpg',
+        channelName: 'Mr. Beast',
+    },
+    {
+        id: 9,
+        thumbnail: '@/assets/mock/Every Minute One Person Is Eliminated.png',
+        title: 'Every Minute One Person Is Eliminated',
+        channelProfilePic: '@/assets/mock/MrBeastChannelPic.jpg',
+        channelName: 'Mr. Beast',
+    },
+]
+
+export default mockVideos
