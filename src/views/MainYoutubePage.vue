ea7c69d685735e5f6d7f7a7917b1edb00f0e2c22
Pedro Passos <pedropassos.yt@gmail.com>
2025-02-08 20:24:57 -0500

    add main youtube page

---
+++ src/views/MainYoutubePage.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="main-youtube-grid">
+        <YourYoutubeThumbnail />
+        <FullYoutubeThumbnail v-for="video in mockVideos" :key="video.id" :video="video" />
+    </div>
+</template>
+
+<script>
+import mockVideos from '@/mock/mockVideos';
+import FullYoutubeThumbnail from '@/components/ThumbnailPreviewPage/FullYoutubeThumbnail.vue';
+import YourYoutubeThumbnail from '@/components/ThumbnailPreviewPage/YourYoutubeThumbnail.vue';
+
+export default {
+    name: "MainYoutubePage",
+    components: {
+        FullYoutubeThumbnail,
+        YourYoutubeThumbnail,
+    },
+    data() {
+        return {
+            mockVideos,
+        }
+    },
+    mounted() {
+        console.log(this.mockVideos);
+    }
+}
+</script>
+
+<style>
+.main-youtube-grid {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    padding: 5rem;
+    justify-content: space-around;
+    max-width: 70%;
+}
+</style>
