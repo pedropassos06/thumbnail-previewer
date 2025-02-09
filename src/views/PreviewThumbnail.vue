2dbb39c6b758be98acecaf75ee752922784e32b8
Pedro Passos <pedropassos.yt@gmail.com>
2025-02-08 20:24:47 -0500

    include main youtube page in preview thumbnail

--- src/views/PreviewThumbnail.vue
+++ src/views/PreviewThumbnail.vue
@@ -1,12 +1,14 @@
 <template>
     <div class="preview-thumbnail-page">
         <LeftMenu class="left-menu" :thumbnails="thumbnails" />
+        <MainYoutubePage />
     </div>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
 import LeftMenu from "@/components/ThumbnailPreviewPage/LeftMenu.vue";
+import MainYoutubePage from "./MainYoutubePage.vue";
 
 export default {
     computed: {
@@ -17,6 +19,7 @@ export default {
     },
     components: {
         LeftMenu,
+        MainYoutubePage,
     },
 }
 </script>
@@ -29,6 +32,7 @@ export default {
     height: 100vh;
     background-color: #f5f5f5;
     color: black;
+    padding: 3rem;
 }
 
 .left-menu {
