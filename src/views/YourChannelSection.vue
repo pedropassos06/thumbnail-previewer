e4bdc64d8bf86b17f928cf7fb0c5366ffcc2b635
Pedro Passos <pedropassos.yt@gmail.com>
2025-02-13 15:43:26 -0500

    create base structure for YourChannelSection

--- src/views/YourChannelSection.vue
+++ src/views/YourChannelSection.vue
@@ -1,16 +1,31 @@
 <template>
     <div class="your-channel-section">
         <SectionTitle>Your Channel</SectionTitle>
+        <div class="your-channel-section-wrapper">
+            <InputBox type="text" placeholder="ex: @pedropassos_" />
+            <ActionButton 
+                width="100%"
+                backgroundColor="#FF0000" 
+                textColor="#FFFFFF" 
+                stroke="#FF0000" 
+                hasDropShadow
+            >Search my channel
+            </ActionButton>
+        </div>
     </div>
 </template>
 
 <script>
 import SectionTitle from '@/components/SectionTitle.vue';
+import InputBox from '@/components/InputBox.vue';
+import ActionButton from '@/components/ActionButton.vue';
 
 export default {
     name: "YourChannelSection",
     components: {
         SectionTitle,
+        InputBox,
+        ActionButton,
     },
 }
 </script>
@@ -19,5 +34,13 @@ export default {
 .your-channel-section {
     width: 100%;
     color: black;
+}
+
+.your-channel-section-wrapper {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
 }
 </style>
