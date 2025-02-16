ecb84a251c6aeb5d92c141ad9ce3524edd1c9cab
Pedro Passos <pedropassos.yt@gmail.com>
2025-02-15 19:26:20 -0500

    feat: add channel module to Vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,13 @@ import { createStore } from 'vuex';
 import thumbnails from './modules/thumbnails';
 import ui from './modules/ui'
 import titles from './modules/titles'
+import channel from './modules/channel'
 
 export default createStore({
     modules: {
         thumbnails,
         ui,
         titles,
+        channel,
     }
 });
