e13c5621178a6726f4a962dbe2ebc9f139737f8d
Pedro Passos <pedropassos.yt@gmail.com>
2025-02-27 19:46:05 -0500

    feat: add remove fa icon

--- src/components/icons/fontAwesome.js
+++ src/components/icons/fontAwesome.js
@@ -2,9 +2,9 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 /* Import only the icons you need */
-import { faTrash, faPlus, faEdit, faCheck } from "@fortawesome/free-solid-svg-icons";
+import { faTrash, faPlus, faEdit, faCheck, faRemove } from "@fortawesome/free-solid-svg-icons";
 
 /* Add icons to the library */
-library.add(faTrash, faPlus, faEdit, faCheck);
+library.add(faTrash, faPlus, faEdit, faCheck, faRemove);
 
 export { FontAwesomeIcon };
