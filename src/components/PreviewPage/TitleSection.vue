e6de0aeb6938b07d2f822e2dd1247d02362f98c7
Pedro Passos <pedropassos.yt@gmail.com>
2025-03-05 18:53:38 -0500

    only show up to 3 titles

--- src/components/PreviewPage/TitleSection.vue
+++ src/components/PreviewPage/TitleSection.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="w-full text-black gap-2.5">
         <SectionTitle>Titles</SectionTitle>
-        <div class="flex flex-col gap-2.5 max-h-40 overflow-y-auto py-2">
+        <div class="flex flex-col gap-2.5">
             <div v-for="(_, index) in titles" class="flex items-center w-full gap-2.5">
                 <InputBox v-model="titles[index].value" placeholder="Enter title..." :selected="titles[index].isSelected" @click="handleSelectTitle(index)"/>
                 <font-awesome-icon v-if="showDeleteButton" class="cursor-pointer text-gray-500 hover:text-red-500" icon="fa-solid fa-trash" @click="handleDeleteTitle(index)"/>
             </div>
+            <ActionButton v-if="showAddButton" type="tertiary" @click="handleAddTitle"><font-awesome-icon icon="fa-solid fa-plus" /></ActionButton>
         </div>
-        <ActionButton type="tertiary" @click="handleAddTitle"><font-awesome-icon icon="fa-solid fa-plus" /></ActionButton>
     </div>
 </template>
 
@@ -31,6 +31,9 @@ export default {
         },
         showDeleteButton() {
             return this.titles.length > 1;
+        },
+        showAddButton() {
+            return this.titles.length < 3;
         },
     },
     methods: {
